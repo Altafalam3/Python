7f768c3828e38507f9f4c8d26ebacb5a64e1d86c
Altaf Alam <102013452+Altafalam3@users.noreply.github.com>
2022-12-04 15:30:41 +0530

    Update 1_Intro.py

--- 1_Intro.py
+++ 1_Intro.py
@@ -9,9 +9,11 @@ print(os.listdir()) #contents of directory
 #we can use python as a calculator
 #enter just python in cmd
 
-#Dynamically typed,OOPS as well functional approach supports
-#Interpreted high level programming language
-#created in 1989 by Guido van Rossum
+'''
+Dynamically typed,OOPS as well functional approach supports
+Interpreted high level programming language
+created in 1989 by Guido van Rossum
 
-#data visualisation to create plots,graphical 
-#data analytics , AI ML , web applications , handle database
+data visualisation to create plots,graphical 
+data analytics , AI ML , web applications , handle database
+'''
